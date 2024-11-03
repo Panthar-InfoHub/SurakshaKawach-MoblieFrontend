8878ce628b9413af6a5026d901a4d225978b6ac1
Arqam Ahmad Siddiqui <arqamahmad365.au@gmail.com>
2024-11-03 14:43:31 +0530

    implemented emergency dashboard to view victim location from app also

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nextlevelprogrammers.surakshakawach"
         minSdk = 27
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.3.1"
+        versionCode = 9
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/nextlevelprogrammers/surakshakawach/api/TicketDetailsResponse.kt
+++ app/src/main/java/com/nextlevelprogrammers/surakshakawach/api/TicketDetailsResponse.kt
@@ -21,3 +21,25 @@ data class Coordinates(
     val latitude: Double,
     val longitude: Double
 )
+
+@Serializable
+data class ApiResponse(
+    val message: String,
+    val data: DataContent
+)
+
+@Serializable
+data class DataContent(
+    val user: UserInfo, // Define if you need user data, otherwise you can skip it
+    val ticket: TicketDetailsResponse
+)
+@Serializable
+data class UserInfo(
+    val firebaseUID: String,
+    val displayName: String,
+    val email: String,
+    val gender: String,
+    val emergencyContacts: List<EmergencyContact>,
+    val tickets: List<String>
+)
+
