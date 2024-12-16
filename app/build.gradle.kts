7251a2b1421e0b7fe866290a8ffa0e0abe44df8e
Arqam Ahmad Siddiqui <arqamahmad365.au@gmail.com>
2024-12-16 09:04:25 +0530

    updated gradle for version to make app live to play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nextlevelprogrammers.surakshakawach"
         minSdk = 27
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.2.6"
+        versionCode = 15
+        versionName = "1.4.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
